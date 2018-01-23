0441d43cd6bbf7c30a321db6bc06c45f39b1bc4c
Chris Palmeri <chrispalmeri@gmail.com>
2018-01-22 21:06:42 -0600

    Make pattern a grid

--- src/js/pattern.js
+++ src/js/pattern.js
@@ -8,18 +8,35 @@ export default new function() {
     var pattern = document.createElementNS("http://www.w3.org/2000/svg", "pattern");
 
     pattern.setAttribute('id', 'pattern');
-    pattern.setAttribute('x', 0);
-    pattern.setAttribute('y', 0);
-    pattern.setAttribute('width', 20);
-    pattern.setAttribute('height', 20);
+    pattern.setAttribute('x', -12.5);
+    pattern.setAttribute('y', -12.5);
+    pattern.setAttribute('width', 96);
+    pattern.setAttribute('height', 96);
     pattern.setAttribute('patternUnits', 'userSpaceOnUse');
 
-    var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-    circle.setAttribute('cx', 10);
-    circle.setAttribute('cy', 10);
-    circle.setAttribute('r', 10);
-    circle.setAttribute('fill', '#393');
-    pattern.appendChild(circle);
+    var g = document.createElementNS("http://www.w3.org/2000/svg", "g");
+    g.setAttribute('stroke-width', 1);
+    g.setAttribute('stroke', '#00f');
+    g.setAttribute('transform', 'translate(-0.5, -0.5)');
+    pattern.appendChild(g);
+
+    // stroke width 1/96 * 100
+
+    for (var i = 0; i < 4; i += 1) {
+      var horizontal = document.createElementNS("http://www.w3.org/2000/svg", "line");
+      horizontal.setAttribute('x1', 0);
+      horizontal.setAttribute('y1', (24 * i) + 12);
+      horizontal.setAttribute('x2', 97);
+      horizontal.setAttribute('y2', (24 * i) + 12);
+      g.appendChild(horizontal);
+
+      var vertical = document.createElementNS("http://www.w3.org/2000/svg", "line");
+      vertical.setAttribute('x1', (24 * i) + 12);
+      vertical.setAttribute('y1', 0);
+      vertical.setAttribute('x2', (24 * i) + 12);
+      vertical.setAttribute('y2', 97);
+      g.appendChild(vertical);
+    }
 
     defs.appendChild(pattern);
     canvas.element.appendChild(defs);
